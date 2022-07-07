d7a1100184ef93afee618343c98abb6ea837360e
maxixking66 <maxixking66@gamil.com>
2022-07-07 10:15:22 +0430

    read from file with fileReader

--- src/com/maktabsharif74/io/JavaIOTutorialApplication.java
+++ src/com/maktabsharif74/io/JavaIOTutorialApplication.java
@@ -1,14 +1,34 @@
 package com.maktabsharif74.io;
 
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class JavaIOTutorialApplication {
 
     public static void main(String[] args) throws IOException {
-        writeOnFile();
+//        writeOnFile();
 
+//        readFileWithFileReader();
+
+    }
+
+    private static void readFileWithFileReader() throws IOException {
+        FileReader fileReader = new FileReader("test-char.txt");
+        int ch;
+        while (-1 != (ch = fileReader.read())) {
+            System.out.println((char) ch);
+        }
+        /*int ch = 0;
+        while (ch != -1) {
+            ch = fileReader.read();
+            if (ch != -1) {
+                System.out.println((char) ch);
+            }
+        }*/
+
+        fileReader.close();
     }
 
     private static void writeOnFile() throws IOException {
