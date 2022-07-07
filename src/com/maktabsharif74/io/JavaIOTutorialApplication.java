083315a63de620d54ac1f5ac2df9ce82fcbcdd0a
maxixking66 <maxixking66@gamil.com>
2022-07-07 09:54:32 +0430

    write on file

--- src/com/maktabsharif74/io/JavaIOTutorialApplication.java
+++ src/com/maktabsharif74/io/JavaIOTutorialApplication.java
@@ -1,8 +1,24 @@
 package com.maktabsharif74.io;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class JavaIOTutorialApplication {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        writeOnFile();
 
+    }
+
+    private static void writeOnFile() throws IOException {
+        File file = new File("test-char.txt");
+        System.out.println("file.length: " + file.length());
+
+        FileWriter fileWriter = new FileWriter(file);
+        fileWriter.write("majid");
+        fileWriter.write("asghari zade");
+        fileWriter.close();
+        System.out.println("file.length: " + file.length());
     }
 }
